f3156ec99c4b2038e2dafb120527c075956dce19
alpaca-tc <alpaca-tc@alpaca.tc>
2016-12-25 04:36:00 +0900

    Freeze const object

--- lib/github_markdown_api.rb
+++ lib/github_markdown_api.rb
@@ -2,14 +2,14 @@ require 'github_markdown_api/version'
 
 module GitHubMarkdownAPI
   DEFAULT_SCHEME       = :https
-  DEFAULT_HOST         = 'api.github.com'
+  DEFAULT_HOST         = 'api.github.com'.freeze
   DEFAULT_PORT         = nil
   DEFAULT_ENDPOINTS    = {
     raw:  '/markdown/raw',
     attr: '/markdown',
-  }
+  }.freeze
   DEFAULT_AUTH         = nil
-  DEFAULT_CONTENT_TYPE = 'text/plain'
+  DEFAULT_CONTENT_TYPE = 'text/plain'.freeze
 
   autoload :APIClient, 'github_markdown_api/api_client'
   autoload :Raw, 'github_markdown_api/raw'
